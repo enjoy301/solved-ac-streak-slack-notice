35646fc3f3e808ed21eeca48f63f0afb6dd8eb58
Eunjong Kim <eunjong147@pusan.ac.kr>
2022-03-07 00:25:23 +0900

    FIX datetime to str

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import slack_notice
 import get_streak
 
+
 is_solved, last_solved_time, standard_time = get_streak.main()
 
-slack_notice.main(['NOT SOLVED TODAY', 'https://solved.ac/', is_solved, last_solved_time, standard_time])
+slack_notice.main(['NOT SOLVED TODAY', 'https://solved.ac/', is_solved, last_solved_time.strftime("%Y/%m/%d, %H:%M:%S"), standard_time.strftime("%Y/%m/%d, %H:%M:%S")])
